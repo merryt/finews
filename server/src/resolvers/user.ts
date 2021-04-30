ddb0cfc19593cbd87c874ce336a3251a8256db0b
tmerry <tymerry@gmail.com>
2021-04-30 12:15:19 -0600

    hooking up cookie conneciton

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -99,7 +99,7 @@ export class UserResolver {
   @Mutation(() => UserResponse)
   async login(
     @Arg("options") options: UsernamePasswordInput,
-    @Ctx() { em }: MyContext
+    @Ctx() { em, req }: MyContext
   ): Promise<UserResponse> {
     const user = await em.findOne(User, {
       username: options.username.toLowerCase(),
@@ -126,6 +126,9 @@ export class UserResolver {
         ],
       };
     }
+
+    req.session.userId = user.id;
+
     return { user };
   }
 }
